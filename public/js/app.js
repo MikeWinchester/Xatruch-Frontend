386f9a0515635e1af08ef7cafba91f07d78c1698
Mike Winchester <aleminapavon30@gmail.com>
2023-11-25 23:29:53 -0600

    First commit

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', function () {
+        navLinks.classList.toggle('show');
+    });
+});
